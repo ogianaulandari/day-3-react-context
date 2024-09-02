8e0b5118c21ca81807256f095e252456a85719bd
Ogiana Ulandari <81963416+ogianaulandari@users.noreply.github.com>
2024-09-02 13:14:18 +0800

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="bg-gray-200 text-black text-center py-4">
-            <p>&copy;2022-2025 Pasar Digital UMKM Indonesia</p>
-        </footer>
-    );
-};
-
-export default Footer;
